04a6f0f2e1750c7d00891f400291201350cbc9af
Axel Cano <axel-cano@live.fr>
2024-12-01 18:18:43 +0100

    Adding entry.html and error.html to implement entries visualization via url

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -8,3 +8,20 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    # get the content of the page in an entry variable
+    entry = util.get_entry(title)
+
+    # if no entry, render the error page
+    if not entry:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
+
+    # if entry, redirect to the page
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "entry": entry
+    }
+    )
